f4a946bdee38a7073d52e0df7474e24136abdb1a
Burak Canbul <burakcanbul41@gmail.com>
2024-05-24 14:57:33 +0300

    add api class

---
+++ app/src/main/java/com/example/magichat2/DataApi.java
@@ -0,0 +1,18 @@
+package com.example.magichat2;
+
+public class DataApi {
+    private String url;
+
+    public DataApi(String url){
+        this.url = url;
+
+    }
+    private UserModel getUser(int id){
+        return null;
+    }
+    private void addUser(UserModel user){
+
+    }
+
+
+}
